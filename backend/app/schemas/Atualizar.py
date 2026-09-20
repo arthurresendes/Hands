67419accd0903b834034c40e664392d522a94462
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-09-20 14:02:53 -0300

    Atualizando informações de clientes no hands

---
+++ backend/app/schemas/Atualizar.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class AtualizarCliente(BaseModel):
+    id: str
+    nome: Optional[str] = None
+    email: Optional[str] = None
+    idade: Optional[int] = None
+    cep: Optional[str] = None
+    cpf: Optional[str] = None
+    body: Optional[str] = None
+    senha: Optional[str] = None
+
+class AtualizarPrestador(BaseModel):
+    id: str
+    nome: Optional[str] = None
+    email: Optional[str] = None
+    idade: Optional[int] = None
+    cep: Optional[str] = None
+    cpf: Optional[str] = None
+    body: Optional[str] = None
+    valor: Optional[float] = None
+    senha: Optional[str] = None
+    servicos: Optional[list[str]] = None
+    idiomas: Optional[list[str]] = None
+    certificacoes: Optional[list[str]] = None
--- backend/app/src/mongo/query.py
+++ backend/app/src/mongo/query.py
@@ -1,7 +1,10 @@
 from core.database import cliente_col,prestador_col,db
 #from motor.motor_asyncio import AsyncIOMotorClient
-#from bson import ObjectId
+from bson import ObjectId
+from bson.errors import InvalidId
 from fastapi import HTTPException, status
+from core.security import gerar_senha_hash
+from src.mongo.criacao import validar_cpf,pegar_dados_cep
 
 async def deletar_por_email(collection_name: str, email: str):
     collection = db[collection_name]
@@ -31,4 +34,68 @@ async def listagem_prestadores():
     for d in docs:
             d['_id'] = str(d['_id'])
     return docs
+
+async def busca_especifica_cliente(id: str):
+    try:
+        res = await cliente_col.find_one({"_id": ObjectId(id)}, protecao_dados_sensiveis)
+        if res:
+            res['_id'] = str(res['_id'])
+        return res
+    except InvalidId:
+        return None
+
+async def busca_especifica_prestador(id: str):
+    try:
+        res = await prestador_col.find_one({"_id": ObjectId(id)}, protecao_dados_sensiveis)
+        if res:
+            res['_id'] = str(res['_id'])
+        return res
+    except InvalidId:
+        return None
+
+async def atualizar(collection_name: str,id: str, campo: str, valor):
+    try:
+        collection = db[collection_name]
+        filtro = {"_id": ObjectId(id)}
+        alteracao = {"$set": {campo: valor}}
+        res = await collection.update_one(filtro,alteracao)
+        return res.modified_count > 0
+    except InvalidId:
+        return False
+
+async def atualizar_senha(collection_name: str,id: str, senha: str):
+    try:
+        collection = db[collection_name]
+        filtro = {"_id": ObjectId(id)}
+        alteracao = {"$set": {"senha": gerar_senha_hash(senha)}}
+        res = await collection.update_one(filtro,alteracao)
+        return res.modified_count > 0
+    except InvalidId:
+        return False
+
+async def atualizar_cpf(collection_name: str,id: str, cpf: str):
+    try:
+        collection = db[collection_name]
+        cpf_valido = await validar_cpf(cpf)
+        if cpf_valido != True:
+            return {'Erro': 'CPF não validado, insira novamente.'}
+        filtro = {"_id": ObjectId(id)}
+        alteracao = {"$set": {"cpf": cpf}}
+        res = await collection.update_one(filtro,alteracao)
+        return res.modified_count > 0
+    except InvalidId:
+        return False
+
+async def atualizar_cep(collection_name: str,id: str, cep: str):
+    try:
+        collection = db[collection_name]
+        resultado_cep  = await pegar_dados_cep(cep)
+        if "erro" in resultado_cep:
+            return {"Erro": resultado_cep["erro"]}
+        filtro = {"_id": ObjectId(id)}
+        alteracao = {"$set": {"endereco": [{'cep': cep, 'rua': resultado_cep ['rua'], 'cidade': resultado_cep ['cidade'], 'estado': resultado_cep['estado'], 'latitude': resultado_cep['lat'], 'longitude': resultado_cep['lon']}]}}
+        res = await collection.update_one(filtro,alteracao)
+        return res.modified_count > 0
+    except InvalidId:
+        return False
 
