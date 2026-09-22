9f295f3aa214abb6a274ce62f5ccdbcfbfa49079
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-09-22 00:02:07 -0300

    Landing page - Hands

--- frontend/hands-front/src/App.jsx
+++ frontend/hands-front/src/App.jsx
@@ -1,10 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
 
 function App() {
-
   return (
     <>
-      <h1>Hands</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
